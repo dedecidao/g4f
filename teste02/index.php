a701e6b4447f3817929adb0bb60e5dce04214210
Andre Luis <dedefest@gmail.com>
2023-07-11 20:51:00 -0300

    add teste 2

---
+++ teste02/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste 02 G4F</title>
+</head>
+<style>
+    body {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 
+        'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 
+        'Open Sans', 
+        'Helvetica Neue', sans-serif;
+        text-align: center;
+    }
+
+    h1 {
+        color: #333;
+    }
+
+    .numero {
+        margin-bottom: 5px;
+    }
+</style>
+
+<body>
+    <h1>Números e Múltiplos</h1>
+    <h2 style="display: inline-block;">Candidato: André Luís Araujo</h2>
+    <img src="https://img.shields.io/badge/Status-Em%20Andamento-yellow" alt="Status do Projeto" style=position:relative;>
+
+    
+    <div id="resultado"></div>
+</body>
+<script>
+
+</script>
+
+</html>
