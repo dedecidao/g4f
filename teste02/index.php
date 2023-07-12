38df3d86517f9a6e2b22ffb96c4f13235793e390
Andre Luis <dedefest@gmail.com>
2023-07-11 21:49:33 -0300

    add teste 2

--- teste02/index.php
+++ teste02/index.php
@@ -25,15 +25,41 @@
 </style>
 
 <body>
-    <h1>Números e Múltiplos</h1>
+    <h1>Procura Tabela ASCII</h1>
     <h2 style="display: inline-block;">Candidato: André Luís Araujo</h2>
     <img src="https://img.shields.io/badge/Status-Em%20Andamento-yellow" alt="Status do Projeto" style=position:relative;>
 
     
     <div id="resultado"></div>
-</body>
-<script>
 
-</script>
+    <button onclick="window.location.reload();" style='background-color: #4CAF50; color: white;font-size: 16px; border-radius: 4px; border: none; cursor:pointer;padding: 10px' >Executar</button> 
+
+    <?php 
+    //temporizador
+    $tempo_inicial = microtime(true);
+    //input
+    $arrCharsAscii = array();
+    $arrCharsAscii = range(',', '|');
+    $totalElementos = count($arrCharsAscii);
+
+    //code
+    $elRemovido = array_splice($arrCharsAscii, rand(0, count($arrCharsAscii) - 1), 1);
+    $somaEsperada = $totalElementos * ($totalElementos + 1) / 2; //formula da soma de PA
+    $somaAtual = array_sum(array_map('ord',$arrCharsAscii)); 
+    $elFaltante = $somaEsperada - $somaAtual;
+    //pause
+    $tempo_final = microtime(true);
+    $tempo = $tempo_final - $tempo_inicial;
+    //output
+    echo '<h3>Elementos da tabela ASCII:</h3>';
+    foreach ($arrCharsAscii as $key => $value) {
+        echo "<span class='numero'>$value</span>";
+    }
+    echo "<h3>Elemento removido: <span style='color: red;'>$elRemovido[0]</span></h3>";
+    echo "<h3>Tempo de execução: <span style='color: red;'>" . number_format($tempo, 6) . "</span> segundos</h3>";
+
+     ?>
+</body>
+
 
 </html>
